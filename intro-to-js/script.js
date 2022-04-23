1d6aaa9683396c783f38b68f72d0f790470b217a
jenguyenanhnhu <102585311+jenguyenanhnhu@users.noreply.github.com>
2022-04-23 12:31:32 +1000

    JavaScript and Keyboard
    23/04/22

--- intro-to-js/script.js
+++ intro-to-js/script.js
@@ -1,6 +1,18 @@
 window.onload = function() {
     // start
+let starterDiv = document.getElementById('starter-div');
+let firstHeading = document.createElement('h2');
+firstHeading.innerText = 'This is a H2 started with JavaScript!';
+firstHeading.classList.add('js-header');
 
+starterDiv.appendChild(firstHeading);
+
+let secondHeading = document.createElement('h2')
+secondHeading.innerText = "This is another H2 created with JS!";
+secondHeading.classList.add('js-header')
+secondHeading.id = 'second-h2'
+
+starterDiv.appendChild(secondHeading);
     // end
 }
 
@@ -8,13 +20,14 @@ window.onload = function() {
 function showName(){
     console.log('showName');
     // start
-
+let name = document.getElementById('name').value
+document.getElementById('greeting-text').innerText = 'Hi' + name;
     // end
 }
 
 function changeColour(colour) {
     // start
-
+document.body.style.backgroundColor = colour;
     // end
 }
 
@@ -24,6 +37,7 @@ function resetColour() {
 
 function toggleLightbulb() {
     // start
-
+let lightbulb = document.getElementById('light');
+lightbulb.classList.toggle("on");
     // end
 }
--- keyboard/script.js
+++ keyboard/script.js
@@ -1,12 +1,19 @@
 window.onload = function() {
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     // start
-
     // end
 
     alphabet.forEach(letter => {
         // start
-
+let keyButton = document.createElement('input');
+keyButton.type = 'button';
+keyButton.value = letter;
+keyButton.classList.add('key');
+keyButton.id = letter;
+keyButton.addEventListener('click', event => {
+    highlightLetter(event.target.id)
+})
+keyboard.appendChild(keyButton);
         // end
     });
 }
@@ -15,9 +22,8 @@ window.onload = function() {
 
 // end
 
-
 function highlightLetter(letter){
     // start
-
+console.log (letter)
     // end
 }
